03f73a33e2845e0bdc10173d1b235ebb489a710c
jgali <jaumegali@gmail.com>
2014-01-17 15:27:05 +0100

    Optimización de busqueda si no existe

--- src/com/galix/linguam/activity/Home.java
+++ src/com/galix/linguam/activity/Home.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.galix.linguam.activity;
 
 import java.io.UnsupportedEncodingException;
@@ -12,9 +11,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +25,7 @@ import com.galix.linguam.LinguamApplication;
 import com.galix.linguam.R;
 import com.galix.linguam.db.OriginalWordDBAdapter;
 import com.galix.linguam.db.TranslationDBAdapter;
+import com.galix.linguam.pojo.OriginalWord;
 import com.galix.linguam.pojo.TranslatedWord;
 import com.galix.linguam.util.WordReferenceUtil;
 import com.galix.linguam.util.WordReferenceUtil.Term;
@@ -71,11 +69,13 @@ public class Home extends Activity {
 
 						@Override
 						public void onResponse(JSONObject response) {
-
+							
+							Toast toast = null;
+							
 							try {
 								hashmapResponse = wrUtil
 										.parseJSON(response.toString());
-								TextView result_translate = (TextView) findViewById(R.id.result_translate);
+								//TextView result_translate = (TextView) findViewById(R.id.result_translate);
 								//Get both response WR lists from hashmap
 								List<Term> firstTranslation = hashmapResponse.get("firstTranslation");
 								List<Term> originalTerm = hashmapResponse.get("originalTerm");
@@ -83,20 +83,24 @@ public class Home extends Activity {
 								int firstTranslationSize = firstTranslation.size();
 								//int originalTermSize = originalTerm.size();
 								
-								originalWordDB.createOriginalWord(originalTerm.get(originalTerm.size()-1));
-	
+								OriginalWord originalword = originalWordDB.createOriginalWord(originalTerm.get(originalTerm.size()-1));
 								
-								//Insert to DB
-								for (Term translation : firstTranslation) {
-									 if (--firstTranslationSize == 0) {
-										 translatedWordDB.createTranslation(translation,true);
-									 }else{
-										 translatedWordDB.createTranslation(translation,false);
-									 }
-								}
+								if (originalword != null){
+									//Insert to DB
+									for (Term translation : firstTranslation) {
+										 if (--firstTranslationSize == 0) {
+											 translatedWordDB.createTranslation(translation,true,originalword.getTerm());
+										 }else{
+											 translatedWordDB.createTranslation(translation,false,originalword.getTerm());
+										 }
+									}
 								
 								String translated_word = hashmapResponse.get("firstTranslation").get(firstTranslation.size()-1).getTerm().toString();
-								Toast toast = Toast.makeText(LinguamApplication.getContext(), "Your translated word is: " + translated_word, Toast.LENGTH_LONG);
+								    toast = Toast.makeText(LinguamApplication.getContext(), "Your translated word is: " + translated_word, Toast.LENGTH_LONG);
+								}else{
+									toast = Toast.makeText(LinguamApplication.getContext(), "This word is already in your translated collection words", Toast.LENGTH_LONG);
+								}
+								
 								toast.show();
 								
 								
@@ -135,167 +139,15 @@ public class Home extends Activity {
 		RequestQueue queue = Volley.newRequestQueue(this);
 		EditText translate_caption = (EditText) (findViewById(R.id.search));
 		String word_to_translate = translate_caption.getText().toString();
-		queue.add(this.callWR(word_to_translate));
-
-		// Instance of adapter
-
-		// TODO insert parsing hasmap of request
-		// translateTableAdapter.createTranslation(new(Term()))();
-	}
-
-}
-=======
-package com.galix.linguam.activity;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.List;
-
-import org.json.JSONObject;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.text.InputFilter.LengthFilter;
-import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import com.galix.linguam.LinguamApplication;
-import com.galix.linguam.R;
-import com.galix.linguam.db.OriginalWordDBAdapter;
-import com.galix.linguam.db.TranslationDBAdapter;
-import com.galix.linguam.pojo.TranslatedWord;
-import com.galix.linguam.util.WordReferenceUtil;
-import com.galix.linguam.util.WordReferenceUtil.Term;
-
-public class Home extends Activity {
-
-	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-	
-	private static OriginalWordDBAdapter originalWordDB;
-	private static TranslationDBAdapter translatedWordDB;
-	
-	private String url_base = "http://api.wordreference.com/0.8/6cd19/json";
-	private String languageSource = "en";
-	private String languageTo = "es";
-	private WordReferenceUtil wrUtil;
-	private HashMap<String, List<Term>> hashmapResponse;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		wrUtil = new WordReferenceUtil();
-		originalWordDB = new OriginalWordDBAdapter(LinguamApplication.getContext());
-		translatedWordDB = new TranslationDBAdapter(LinguamApplication.getContext());
-		LinguamApplication.init();
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_layout);
-	}
-
-	public JsonObjectRequest callWR(String word) {
+		TranslatedWord is_translated = translatedWordDB.getTranslateByWord(word_to_translate);
 		
-		JsonObjectRequest jsObjRequest = null;
-
-		try {
-			// Encode word to translate
-			String encoded_word = URLEncoder.encode(word, "UTF-8");
-			// Call URL WR to translate
-			String url = url_base + "/" + languageSource + languageTo + "/"
-					+ encoded_word;
-
-			jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-					new Response.Listener<JSONObject>() {
-
-						private long originalWordId;
-
-						@Override
-						public void onResponse(JSONObject response) {
-
-							try {
-								hashmapResponse = wrUtil
-										.parseJSON(response.toString());
-								TextView result_translate = (TextView) findViewById(R.id.result_translate);
-								//Get both response WR lists from hashmap
-								List<Term> firstTranslation = hashmapResponse.get("firstTranslation");
-								List<Term> originalTerm = hashmapResponse.get("originalTerm");
-								
-								int firstTranslationSize = firstTranslation.size();
-								//int originalTermSize = originalTerm.size();
-								
-								originalWordId = originalWordDB.createOriginalWord(originalTerm.get(originalTerm.size()-1));
-								
-//								for (Term originalWord : originalTerm) {
-//									originalWordId = originalWordDB.createOriginalWord(originalWord);
-//								}
-								//Insert to DB
-								for (Term translation : firstTranslation) {
-									 if (--firstTranslationSize == 0) {
-										 translatedWordDB.createTranslation(translation,true,originalWordId);
-									 }else{
-										 translatedWordDB.createTranslation(translation,false,originalWordId);
-									 }
-								}
-								
-								String translated_word = hashmapResponse.get("firstTranslation").get(firstTranslation.size()-1).getTerm().toString();
-								Toast toast = Toast.makeText(LinguamApplication.getContext(), "Your translated word is: " + translated_word, Toast.LENGTH_LONG);
-								toast.show();
-								//result_translate.setText(hashmapResponse.get("firstTranslation").get(firstTranslation.size()-1).getTerm().toString());
-								//result_translate.setVisibility(1);
-								
-					
-								
-								// findViewById(R.id.progressBar1).setVisibility(View.GONE);
-							} catch (Exception e) {
-								// TODO Auto-generated catch block
-								Log.v("Exception", e.toString());
-								TextView result_translate = (TextView) findViewById(R.id.result_translate);
-								result_translate.setText("No results");
-								result_translate.setVisibility(1);
-							}
-
-						}
-					}, new Response.ErrorListener() {
-
-						@Override
-						public void onErrorResponse(VolleyError error) {
-							Log.v("onErrorResponse - Volley", error.toString());
-							TextView result_translate = (TextView) findViewById(R.id.result_translate);
-							result_translate
-									.setText("There is any network problem - Please check if you have interent access");
-							result_translate.setVisibility(1);
-						}
-					});
-
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (is_translated == null) { //If word dosen't exist, search & save
+			queue.add(this.callWR(word_to_translate));
+		}else{ // If word exist, just show it
+			Toast toast = Toast.makeText(LinguamApplication.getContext(), "Your translated word is:" + is_translated.getTerm(), Toast.LENGTH_LONG);			
+			toast.show();
 		}
 
-		return jsObjRequest;
-	}
-
-	public void translate(View view) {
-
-		RequestQueue queue = Volley.newRequestQueue(this);
-		EditText translate_caption = (EditText) (findViewById(R.id.search));
-		String word_to_translate = translate_caption.getText().toString();
-		queue.add(this.callWR(word_to_translate));
-
-		// Instance of adapter
-
-		// TODO insert parsing hasmap of request
-		// translateTableAdapter.createTranslation(new(Term()))();
 	}
 
 }
->>>>>>> d300226ca0a6d21041db95927a051b702a319ecd
--- src/com/galix/linguam/db/TranslationDBAdapter.java
+++ src/com/galix/linguam/db/TranslationDBAdapter.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.galix.linguam.db;
 
 import java.util.ArrayList;
@@ -24,6 +23,7 @@ public class TranslationDBAdapter {
 			MySQLiteHelper.COLUMN_TRANSLATION_SENSE,
 			MySQLiteHelper.COLUMN_TRANSLATION_TERM,
 			MySQLiteHelper.COLUMN_TRANSLATION_USAGE,
+			MySQLiteHelper.COLUMN_TRANSLATION_ORIGINALWORD,
 			MySQLiteHelper.COLUMN_TRANSLATION_SELECTED};
 
 	public TranslationDBAdapter(Context context) {
@@ -31,10 +31,9 @@ public class TranslationDBAdapter {
 		database = dbHelper.getWritableDatabase(); 
 	}
 
-	public TranslatedWord createTranslation(Term translation,boolean selected) {
+	public TranslatedWord createTranslation(Term translation,boolean selected,String originalword) {
 
 		ContentValues values = new ContentValues();
-
 		
 		values.put(MySQLiteHelper.COLUMN_TRANSLATION_POS, translation.getPOS() == null ? "Empty Pos" : translation.getPOS());
 		values.put(MySQLiteHelper.COLUMN_TRANSLATION_SENSE,
@@ -43,6 +42,8 @@ public class TranslationDBAdapter {
 				translation.getTerm());
 		values.put(MySQLiteHelper.COLUMN_TRANSLATION_USAGE,
 				translation.getUsage() == null ? "Empty Usage" : translation.getUsage());
+		values.put(MySQLiteHelper.COLUMN_TRANSLATION_ORIGINALWORD,
+				originalword);
 		values.put(MySQLiteHelper.COLUMN_TRANSLATION_SELECTED,
 			    selected ? 1 : 0);
 
@@ -116,117 +117,35 @@ public class TranslationDBAdapter {
 		translation.setTerm(cursor.getString(1));
 		return translation;
 	}
-
-	public void open() throws SQLException {
-		database = dbHelper.getWritableDatabase();
-	}
-
-	public void close() {
-		dbHelper.close();
-	}
-
-=======
-package com.galix.linguam.db;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import com.galix.linguam.pojo.TranslatedWord;
-import com.galix.linguam.util.WordReferenceUtil.Term;
-
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
-
-public class TranslationDBAdapter {
-
-	// Database fields
-	private SQLiteDatabase database;
-	private MySQLiteHelper dbHelper;
-	private String[] allColumns = { MySQLiteHelper.COLUMN_TRANSLATION_ID,
-			MySQLiteHelper.COLUMN_TRANSLATION_POS,
-			MySQLiteHelper.COLUMN_TRANSLATION_SENSE,
-			MySQLiteHelper.COLUMN_TRANSLATION_TERM,
-			MySQLiteHelper.COLUMN_TRANSLATION_USAGE,
-			MySQLiteHelper.COLUMN_TRANSLATION_SELECTED};
-
-	public TranslationDBAdapter(Context context) {
-		dbHelper = new MySQLiteHelper(context);
-		database = dbHelper.getWritableDatabase(); 
-	}
-
-	public long createTranslation(Term translation,boolean selected,long originalWordId) {
-
-		ContentValues values = new ContentValues();
-
-		
-		values.put(MySQLiteHelper.COLUMN_TRANSLATION_POS, translation.getPOS() == null ? "Empty Pos" : translation.getPOS());
-		values.put(MySQLiteHelper.COLUMN_TRANSLATION_SENSE,
-				translation.getSense() == null ? "Empty Sense" : translation.getSense());
-		values.put(MySQLiteHelper.COLUMN_TRANSLATION_TERM,
-				translation.getTerm());
-		values.put(MySQLiteHelper.COLUMN_TRANSLATION_USAGE,
-				translation.getUsage() == null ? "Empty Usage" : translation.getUsage());
-		values.put(MySQLiteHelper.COLUMN_TRANSLATION_SELECTED,
-			    selected ? 1 : 0);
-		values.put(MySQLiteHelper.COLUMN_TRANSLATION_ORIGINALWORD_FK,
-				originalWordId != 0 ? originalWordId : 0);
-
-		long insertId = database.insert(MySQLiteHelper.TABLE_TRANSLATION, null,
-				values);
-		return insertId;
-	}
-
-	public void deleteTranslation(TranslatedWord translation) {
-		long id = translation.getId();
-		System.out.println("Comment deleted with id: " + id);
-		database.delete(MySQLiteHelper.TABLE_TRANSLATION,
-				MySQLiteHelper.COLUMN_TRANSLATION_ID + " = " + id, null);
-	}
-
-	public List<TranslatedWord> getAllTranslates() {
-		List<TranslatedWord> translationWords = new ArrayList<TranslatedWord>();
-
-		Cursor cursor = database.query(MySQLiteHelper.TABLE_TRANSLATION,
-				allColumns, null, null, null, null, null);
-
-		cursor.moveToFirst();
-		while (!cursor.isAfterLast()) {
-			TranslatedWord translation = cursorToTranslate(cursor);
-			translationWords.add(translation);
-			cursor.moveToNext();
-		}
-		// make sure to close the cursor
-		cursor.close();
-		return translationWords;
-	}
-
-	public String selectedTranslation(){
-		
-		String[] fields = new String[] {"term"};
-		String[] args = new String[] {"1"};
-		 
-		Cursor c = database.query(MySQLiteHelper.TABLE_TRANSLATION, fields, MySQLiteHelper.COLUMN_TRANSLATION_SELECTED+"=?",
-				args, null, null, null);
-		
-		//Nos aseguramos de que existe al menos un registro
-		if (c.moveToFirst()) {
-		     //Recorremos el cursor hasta que no haya m‡s registros
-		     do {
-		          return c.getString(0);
-		     } while(c.moveToNext());
-		}else{
-			return null;
-		}
-	}
 	
-	private TranslatedWord cursorToTranslate(Cursor cursor) {
+	public TranslatedWord getTranslateByWord(String originalword){
+		
+		List<TranslatedWord> translationWords = new ArrayList<TranslatedWord>();
 		TranslatedWord translation = new TranslatedWord();
-		translation.setId((int) cursor.getLong(0));
-		translation.setTerm(cursor.getString(1));
-		return translation;
+		
+		String selection = "originalword=? AND selected=?";
+    	String[] selectionArgs = {originalword,"1"};
+    	String tableName = MySQLiteHelper.TABLE_TRANSLATION;
+    	
+    	Cursor cursor = database.query(tableName, null, selection, selectionArgs, null, null, null);
+    	if (cursor != null && cursor.getCount() > 0){
+    		cursor.moveToFirst();
+	    	while (!cursor.isAfterLast()) {
+				translation = cursorToTranslate(cursor);
+				translationWords.add(translation);
+				cursor.moveToNext();
+			}
+			
+	    	// make sure to close the cursor
+			cursor.close();
+			return translationWords.get(0);
+    	}else{
+    		cursor.close();
+    		return null;
+    	}
+		
+		
+		
 	}
 
 	public void open() throws SQLException {
@@ -236,6 +155,4 @@ public class TranslationDBAdapter {
 	public void close() {
 		dbHelper.close();
 	}
-
->>>>>>> d300226ca0a6d21041db95927a051b702a319ecd
 }
